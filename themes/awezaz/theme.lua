58db7777d0a1e1ecb820456424645e83794ec321
ZaiZu <zaizu3000@gmail.com>
2017-06-06 22:33:39 +0000

    theme: titlebar icons fix, new colors for levels

--- themes/awezaz/theme.lua
+++ themes/awezaz/theme.lua
@@ -15,6 +15,7 @@ theme.font      = "xos4 Terminus bold 14"   -- arch linux
 --theme.font      = "sans 14"
 
 
+-- {{{ Colors
 theme.bg_normal     = "#050608"
 theme.bg_focus      = "#FF8700"
 theme.bg_urgent     = "#FF7F27"
@@ -37,6 +38,11 @@ theme.fg_widget        = "#908884"
 theme.fg_center_widget = "#636363"
 theme.fg_end_widget    = "#ffffff"
 theme.fg_off_widget    = "#22211f"
+
+theme.level_fg_good     = "#FFaa00"
+theme.level_fg_normal     = "#FF5000"
+theme.level_fg_critical     = "#FF1000"
+
 
 -- {{{ Colors
 --theme.fg_normal = "#DCDCCC"
@@ -64,7 +70,7 @@ theme.titlebar_bg_normal = "#754000"
 -- defined, the sets are:
 -- --[taglist|tasklist]_[bg|fg]_[focus|urgent]
 -- taglist_[bg|fg]_[focus|urgent|occupied|empty]
--- tasklist_[bg|fg]_[focus|urgent]  
+-- tasklist_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
@@ -129,25 +135,29 @@ theme.layout_floating   = "/usr/share/awesome/themes/awezaz/layouts/floating.png
 theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/awezaz/titlebar/close_focus.png"
 theme.titlebar_close_button_normal = "/usr/share/awesome/themes/awezaz/titlebar/close_normal.png"
 
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/ontop_normal_inactive.png"
-
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/sticky_normal_inactive.png"
-
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/floating_normal_inactive.png"
+theme.titlebar_minimize_button_focus = "/usr/share/awesome/themes/awezaz/titlebar/minimized_focus.png"
+-- theme.titlebar_minimize_button_focus_hover = "/usr/share/awesome/themes/awezaz/titlebar/minimized_focus_hover.png"
+theme.titlebar_minimize_button_normal = "/usr/share/awesome/themes/awezaz/titlebar/minimized_normal.png"
 
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/maximized_focus_active.png"
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/maximized_normal_inactive.png"
+
+-- theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/ontop_focus_active.png"
+-- theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/ontop_normal_active.png"
+-- theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/ontop_focus_inactive.png"
+-- theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/ontop_normal_inactive.png"
+
+-- theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/sticky_focus_active.png"
+-- theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/sticky_normal_active.png"
+-- theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/sticky_focus_inactive.png"
+-- theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/sticky_normal_inactive.png"
+
+-- theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/awezaz/titlebar/floating_focus_active.png"
+-- theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/awezaz/titlebar/floating_normal_active.png"
+-- theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/awezaz/titlebar/floating_focus_inactive.png"
+-- theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/awezaz/titlebar/floating_normal_inactive.png"
 -- }}}
 -- }}}
 
