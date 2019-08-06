44e4096108f48cea42511c78745c301caf456d21
ZaiZu <zaizu3000@gmail.com>
2019-08-06 16:59:41 +0300

    notification added to arch updates checker.

--- updates_arch.lua
+++ updates_arch.lua
@@ -1,6 +1,7 @@
 
 local wibox = require("wibox")
 local awful = require("awful")
+local naughty = require("naughty")
 
 ----                                                                        ----
 updates_arch_box = wibox.widget.textbox()
@@ -10,8 +11,22 @@ updates_arch_box.font = theme.fontTTF
 function updates_arch()
     awful.spawn.easy_async("updates_arch.sh", function(stdout, stderr, reason, exit_code)
         updates_arch_box:set_text(stdout)
+        updates_arch_popup = naughty.notify({
+            text = "arch updates refreshed",
+            timeout = timeout,
+            hover_timeout = 0.5,
+            screen = awful.screen.focused()
+            })
     end)
 end
+
+----                                                                        ----
+updates_arch_buttons = awful.util.table.join(
+    awful.button({ }, 1, function() updates_arch() end),
+    awful.button({ }, 3, function() updates_arch() end)
+    )
+
+updates_arch_box:buttons(updates_arch_buttons)
 
 ----                                                                        ----
 updates_arch()
